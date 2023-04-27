5102edabe437bd18bdafcb40faca4c2e03ca22a5
TechRiderWR <techrider@wingriders.com>
2023-04-27 03:08:40 +0200

    Add JSDoc to applyParamsToScript function

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,14 @@ import init, {apply_params_to_script} from './pkg/apply_params_to_script'
 
 let initialized: Promise<void> | undefined = undefined
 
+/**
+ * On first call this function will initialize the underlying WASM code, expect
+ * a few tens of milliseconds delay.
+ *
+ * @param paramBytes CBOR serialized params, params need to be in a PlutusData array
+ * @param plutusScriptBytes Plutus script CBOR, not double-encoded
+ * @returns Bytes of script with applied parameters
+ */
 export const applyParamsToScript = async (paramBytes: Uint8Array, plutusScriptBytes: Uint8Array) => {
   if (initialized === undefined) {
     // @ts-ignore
